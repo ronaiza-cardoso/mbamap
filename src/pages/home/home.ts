ad750abbcfc1bcaf4182d280b26d72ed329d145f
Ronaiza Cardoso <ronaizacard@gmail.com>
2017-02-26 14:30:54 -0300

    Adding current user location

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
+
+declare var google;
 
 @Component({
-  selector: 'page-home',
+  selector: 'home-page',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
-    
+    this.ionViewLoaded();
+  }
+
+  ionViewLoaded(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    Geolocation.getCurrentPosition().then(position => {
+
+      const latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      const mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      const marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: this.map.getCenter()
+      });
+
+    }, err => {
+      console.log(err);
+    });
+
   }
 
 }
